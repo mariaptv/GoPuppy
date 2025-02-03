2166f613290913685e69fe4e9b89a550dd4af16d
maria.tellez <maria.tellez@sezzle.com>
2025-02-03 08:58:57 -0500

    Adding mods

--- 045-go-mod-tidy-documentation/go.mod
+++ 045-go-mod-tidy-documentation/go.mod
@@ -1,3 +1,5 @@
-module github.com/mariaptv/GoPuppy
+module github.com/mariaptv/GoPuppy/puppy
 
 go 1.23.3
+
+require github.com/mariaptv/GoPuppy v0.0.0-20250203023331-c5668adb6c27 // indirect
--- 045-go-mod-tidy-documentation/main.go
+++ 045-go-mod-tidy-documentation/main.go
@@ -1,17 +1,17 @@
+package main
 
-	package main
+import (
+	"fmt"
 
-	import (
-		"fmt"
-		"github.com/mariaptv/GoPuppy/puppy"
-	)
+	puppy "github.com/mariaptv/GoPuppy"
+)
 
-	func main() {
-		s1 := puppy.Bark()
-		s2 := puppy.Bark()
+func main() {
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
 
-		fmt.Println(s1)
-		fmt.Println(s2)
+	fmt.Println(s1)
+	fmt.Println(s2)
 
-		fmt.Println(puppy.Bark())
-	}
+	fmt.Println(puppy.Bark())
+}
