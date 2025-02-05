f0f57b30f8c088116b5287c1926cb59b1081553f
maria.tellez <maria.tellez@sezzle.com>
2025-02-04 22:17:21 -0500

    Adding big bark

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mariaptv/GoPuppy
 
 go 1.23.3
+
+require github.com/mariaptv/Dog v0.0.0-20250205023732-5e508ed71011 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,18 @@
 package puppy
 
+import dog "github.com/mariaptv/Dog"
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
